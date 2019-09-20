4784b08669c851578e2e3e13c69f52ea12e474f0
sasabrina <sabrinaealvarez@gmail.com>
2019-09-19 21:57:26 -0300

    statefull home contaniner

---
+++ src/containers/Home.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            title:'Home'
+        }
+    }
+
+    changeTitle(newtitle){
+        this.setState({title:newtitle})
+    }   
+
+    render(){
+        return(
+            <React.Fragment>
+                <h1>{this.state.title}</h1>
+                <p>esto es mas texto</p>
+                <a href="!#" onClick={() => this.changeTitle('inicio')}>cambiar</a>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Home
