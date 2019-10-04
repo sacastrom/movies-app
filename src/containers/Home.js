c3f065d01945c174cad1078a9290fb6b5cceee4a
marcreativo <marcreativo@gmail.com>
2019-10-04 19:38:19 -0300

    fetching movies

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import Header from '../components/Header/Header'
 import NavBar from '../components/NavBar/NavBar'
 import Modal from '../components/Modal/Modal'
-import index from '@babel/template'
 
 class Home extends Component {
 	state = {
 		isModalOpen: false,
+		apiKey: 'e3087953b023a4a056fc42d81ebd595d',
+		movies: [],
 		nav: [
-			{ label: 'Home', href: '/home' },
+			{ label: 'Home', href: '/' },
 			{ label: 'Top Rated', href: '/top' },
 			{ label: 'Popular', href: '/popular' }
 		]
@@ -27,16 +28,28 @@ class Home extends Component {
 		this.toggleModal()
 	}
 
+	getMovies = (cat) => {
+		fetch(`https://api.themoviedb.org/3/movie/${cat}?api_key=${this.state.apiKey}`)
+			.then((res) => res.json())
+			.then((res) => this.setState({ movies: res.results }))
+	}
+
+	componentDidMount() {
+		this.getMovies('top_rated')
+	}
+
 	render() {
+		console.log(this.state.movies)
 		return (
 			<React.Fragment>
 				<Header>
 					<NavBar data={this.state.nav} />
 				</Header>
 				<div className={'container'}>
-					<input type={'text'} />
 					<a onClick={() => this.setMovie('Shawshank redemption')}>btn</a>
+					{this.state.movies.map((e, i) => <p key={i}>{e.title}</p>)}
 				</div>
+
 				<Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
 					<p>'oli'</p>
 				</Modal>
